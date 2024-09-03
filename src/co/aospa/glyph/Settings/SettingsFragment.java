AgBKartikey/packages_apps_Glyph

ParanoidGlyph: Add support for auto brightness
Also reconfigure brightness levels for phone(2).

Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

src/co/aospa/glyph/Settings/SettingsFragment.java
@@ -43,6 +43,7 @@ import com.android.settingslib.widget.OnMainSwitchChangeListener;
 import co.aospa.glyph.R;
 import co.aospa.glyph.Constants.Constants;
 import co.aospa.glyph.Manager.SettingsManager;
+import co.aospa.glyph.Utils.ResourceUtils;
 import co.aospa.glyph.Utils.ServiceUtils;
 
 public class SettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
@@ -51,6 +52,7 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
     private MainSwitchPreference mSwitchBar;
 
     private SwitchPreference mFlipPreference;
+    private SwitchPreference mAutoBrightnessPreference;
     private SeekBarPreference mBrightnessPreference;
     private PrimarySwitchPreference mNotifsPreference;
     private PrimarySwitchPreference mCallPreference;
@@ -84,8 +86,19 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         mFlipPreference.setEnabled(glyphEnabled);
         mFlipPreference.setOnPreferenceChangeListener(this);
 
+        mAutoBrightnessPreference = (SwitchPreference) findPreference(Constants.GLYPH_AUTO_BRIGHTNESS_ENABLE);
+        if (ResourceUtils.getString("glyph_light_sensor").isBlank()) {
+            getPreferenceScreen().removePreference(mAutoBrightnessPreference);
+        }
+        mAutoBrightnessPreference.setEnabled(glyphEnabled);
+        mAutoBrightnessPreference.setOnPreferenceChangeListener(this);
+
         mBrightnessPreference = (SeekBarPreference) findPreference(Constants.GLYPH_BRIGHTNESS);
-        mBrightnessPreference.setEnabled(glyphEnabled);
+        if (mAutoBrightnessPreference.isChecked()) {
+            mBrightnessPreference.setEnabled(false);
+        } else {
+            mBrightnessPreference.setEnabled(glyphEnabled);
+        }
         mBrightnessPreference.setMin(1);
         mBrightnessPreference.setMax(Constants.getBrightnessLevels().length);
         mBrightnessPreference.setValue(SettingsManager.getGlyphBrightnessSetting());
@@ -146,6 +159,10 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
             SettingsManager.setGlyphNotifsEnabled(!mNotifsPreference.isChecked());
         }
 
+        if (preferenceKey.equals(Constants.GLYPH_AUTO_BRIGHTNESS_ENABLE)) {
+            mBrightnessPreference.setEnabled(mAutoBrightnessPreference.isChecked());
+        }
+
         if (preferenceKey.equals(Constants.GLYPH_MUSIC_VISUALIZER_ENABLE)) {
             boolean isChecked = mMusicVisualizerPreference.isChecked();
             mFlipPreference.setEnabled(isChecked);
@@ -171,7 +188,8 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         mSwitchBar.setChecked(isChecked);
 
         mFlipPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-        mBrightnessPreference.setEnabled(isChecked);
+        mAutoBrightnessPreference.setEnabled(isChecked);
+        mBrightnessPreference.setEnabled(isChecked && !mAutoBrightnessPreference.isChecked());
         mNotifsPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
         mNotifsPreference.setSwitchEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
         mCallPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());

StudioKeys-Dumps/packages_apps_ParanoidGlyph

Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

src/co/aospa/glyph/Settings/SettingsFragment.java
@@ -44,6 +44,7 @@ import com.android.settingslib.widget.MainSwitchPreference;
 import co.aospa.glyph.R;
 import co.aospa.glyph.Constants.Constants;
 import co.aospa.glyph.Manager.SettingsManager;
+import co.aospa.glyph.Utils.ResourceUtils;
 import co.aospa.glyph.Utils.ServiceUtils;
 
 public class SettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
@@ -52,6 +53,7 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
     private MainSwitchPreference mSwitchBar;
 
     private SwitchPreferenceCompat mFlipPreference;
+    private SwitchPreferenceCompat mAutoBrightnessPreference;
     private SeekBarPreference mBrightnessPreference;
     private PrimarySwitchPreference mNotifsPreference;
     private PrimarySwitchPreference mCallPreference;
@@ -85,8 +87,19 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         mFlipPreference.setEnabled(glyphEnabled);
         mFlipPreference.setOnPreferenceChangeListener(this);
 
+        mAutoBrightnessPreference = (SwitchPreferenceCompat) findPreference(Constants.GLYPH_AUTO_BRIGHTNESS_ENABLE);
+        if (ResourceUtils.getString("glyph_light_sensor").isBlank()) {
+            getPreferenceScreen().removePreference(mAutoBrightnessPreference);
+        }
+        mAutoBrightnessPreference.setEnabled(glyphEnabled);
+        mAutoBrightnessPreference.setOnPreferenceChangeListener(this);
+
         mBrightnessPreference = (SeekBarPreference) findPreference(Constants.GLYPH_BRIGHTNESS);
-        mBrightnessPreference.setEnabled(glyphEnabled);
+        if (mAutoBrightnessPreference.isChecked()) {
+            mBrightnessPreference.setEnabled(false);
+        } else {
+            mBrightnessPreference.setEnabled(glyphEnabled);
+        }
         mBrightnessPreference.setMin(1);
         mBrightnessPreference.setMax(Constants.getBrightnessLevels().length);
         mBrightnessPreference.setValue(SettingsManager.getGlyphBrightnessSetting());
@@ -147,6 +160,10 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
             SettingsManager.setGlyphNotifsEnabled(!mNotifsPreference.isChecked());
         }
 
+        if (preferenceKey.equals(Constants.GLYPH_AUTO_BRIGHTNESS_ENABLE)) {
+            mBrightnessPreference.setEnabled(mAutoBrightnessPreference.isChecked());
+        }
+
         if (preferenceKey.equals(Constants.GLYPH_MUSIC_VISUALIZER_ENABLE)) {
             boolean isChecked = mMusicVisualizerPreference.isChecked();
             mFlipPreference.setEnabled(isChecked);
@@ -172,7 +189,8 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         mSwitchBar.setChecked(isChecked);
 
         mFlipPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-        mBrightnessPreference.setEnabled(isChecked);
+        mAutoBrightnessPreference.setEnabled(isChecked);
+        mBrightnessPreference.setEnabled(isChecked && !mAutoBrightnessPreference.isChecked());
         mNotifsPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
         mNotifsPreference.setSwitchEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
         mCallPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());